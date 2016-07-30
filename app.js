ac02a96efb5641aeaa79c31a60ed71a0e73ce8e2
David Higuita <david.higuita.75@gmail.com>
2016-07-30 17:29:40 -0500

    End point (Basic API) using nodejs. Must fix for chrome.

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+
+var app = express();
+var server = app.listen(3000);
+
+mongoose.connect("mongodb://localhost/endpoint_users");
+
+var controllers = require("./controllers/controllers");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", controllers.app.get_home);
+app.post("/app_test_endpoint/get_users", controllers.app.post_home);
+app.post("/app_test_endpoint/add_user", controllers.app.add_user);
+
+
+module.exports = app;
---
+++ controllers/home.js
@@ -0,0 +1,55 @@
+var users = require("../models/users");
+
+exports.get = function(req, res){
+    res.render("home");
+};
+
+exports.post = function(req, res){
+    console.log("POST");
+    console.log(req.body);
+    //if(req.body["get_users"]=="ok"){
+        var object_to_serve = [
+            {
+                "id": "0",
+                "username": "david",
+                "nickname": "david75",
+                "password": "davidpass"
+            },
+            {
+                "id": "1",
+                "username": "david",
+                "nickname": "david75",
+                "password": "davidpass"
+            },
+            {
+                "id": "2",
+                "username": "david",
+                "nickname": "david75",
+                "password": "davidpass"
+            }
+        ];
+
+        //res.send(object_to_serve);
+        
+        users.find({}, function(err, us){
+            console.log(us);
+            res.send(us);
+        });
+    /*}else{
+        res.send({ans: "error"});
+    }*/
+};
+
+exports.add_user = function(req, res){
+    console.log("NEW USER");
+    console.log(req.body);
+    var new_user = new users({
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        email: req.body.email
+    });
+    new_user.save(function(err){
+        if(!err) console.log("New user added");
+        else console.log("Error saving new user: " + err);
+    });
+};
